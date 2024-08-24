c4d7e4e7d8901b0a59bdc7da2c696510f6c1eac7
rmurarishetti <64725000+rmurarishetti@users.noreply.github.com>
2024-08-24 18:24:44 +0800

    feat:Plan Confirmation Modal Complete

--- src/app/components/ConfirmationModal.js
+++ src/app/components/ConfirmationModal.js
@@ -6,19 +6,18 @@ import React, { useEffect } from "react";
 import { Cross2Icon } from "@radix-ui/react-icons";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
-import styles from "../constants/styles";
 import db from "../utils/firestore";
-import {
-  getDoc,
-  doc,
-} from "firebase/firestore";
+import { LuCheckCircle } from "react-icons/lu";
+import { useUser } from "@auth0/nextjs-auth0/client";
+import { getDoc, doc, query, collection, where, getDocs, serverTimestamp, updateDoc } from "firebase/firestore";
 
 export default function ConfirmationModal({ props }) {
   const router = useRouter();
+  const { user, isLoading } = useUser();
 
   const [open, setOpen] = useState(false);
-
-  const handleSubmit = (e) => {};
+  const [userId, setUserId] = useState("");
+  
 
   const [plan, setPlan] = useState({
     description: "",
@@ -28,7 +27,18 @@ export default function ConfirmationModal({ props }) {
     name: "",
   });
 
+  const userCollection = collection(db, "Users");
+
   async function getPlanDetails() {
+    if (user && user.email){
+      const q = query(userCollection, where("email", "==", user.email))
+      const snapshot = await getDocs(q);
+      if (snapshot.docs.length>0){
+        const userData = snapshot.docs[0].data();
+        setUserId({userid:snapshot.docs[0].id});
+      }
+    }
+
     const docSnap = await getDoc(doc(db, "Plans", props.id));
     if (docSnap.exists) {
       setPlan(docSnap.data());
@@ -39,6 +49,17 @@ export default function ConfirmationModal({ props }) {
 
   const handleOpen = async () => {
     await getPlanDetails().then(setOpen(true));
+  };
+
+  const handleSubmit = async (e) => {
+    const userRef = doc(db, "Users", userId.userid);
+    const docRef = await updateDoc(userRef, {
+      plan:props.id,
+      limit:plan.devices,
+      validity:plan.validity,
+      date: serverTimestamp(),
+    }).then(router.push(`/confirmation`));
+
   };
 
   return (
@@ -62,19 +83,19 @@ export default function ConfirmationModal({ props }) {
             Confirm Your Subscription Plan.
           </Dialog.Title>
           <Dialog.Description className="text-mauve11 mt-[10px] mb-5 text-[15px] leading-normal">
-            Review your plan here. Click submit when you&apos;re done.
+            Review your plan here. Click confirm when you&apos;re done. 
           </Dialog.Description>
 
-          <div className={"mt-[10px] mb-5 text-[15px] " + styles.paragraph}>
+          <div className={"mt-[10px] mb-5 text-[17px] font-medium text-black"}>
             {plan.description == "" ? "Loading" : plan.description}
           </div>
 
-          <div className="mt-[25px] flex justify-end">
+          <div className="mt-[25px] flex justify-center">
             <button
               onClick={handleSubmit}
-              className="bg-[#635BFF] text-white focus:shadow-green7 inline-flex h-[35px] items-center justify-center rounded-[4px] px-[15px] font-medium leading-none focus:shadow-[0_0_0_2px] focus:outline-none"
+              className="bg-black w-full text-white focus:shadow-green7 inline-flex h-[35px] items-center justify-center rounded-[4px] px-[15px] font-medium leading-none focus:shadow-[0_0_0_2px] focus:outline-none"
             >
-              Confirm
+              <LuCheckCircle className="text-green-600 font-bold" /> Confirm
             </button>
           </div>
           <button
